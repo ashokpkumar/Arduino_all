2aab132cedf95f707cc6932ac0503fc8a6261a2d
Ashok kumar <mani120488@gmail.com>
2021-02-23 13:02:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,31 @@ def hello():
     print "hello world"
     return 'Hello World!'
 
-@app.route('/get_firmware_update/<gatewayserial>/<firmwareversion><message>')
+@app.route('/get_firmware_update/<gatewayserial>/<firmwareversion>/<message>')
 def updatefirmware(gatewayserial,firmwareversion,message):
     print "Message type :>>>>>>>>>>>>>>",message
-    if message =="firmwareupdate":    
-        f = open('binfileName','rb')
-        data = f.read()
-        print ">>>>>>>>>>>>>>>>>>>", gatewayserial
-        print "*******************", firmwareversion
-        return data
+    print "gatewayserial :>>>>>>>", gatewayserial
+    print "firmwareversion :********", firmwareversion
+
+    latest_firmware_version = 1.5
+    gateway_serial_op = "at-at212"
+   
+    if message =="firmwareupdate":  
+        if gatewayserial ==gateway_serial_op:     
+            if float(firmwareversion) < latest_firmware_version:
+                print ">>>>>>>>>>>>>>>>>>Firmware found to be outdated. updating the firmware"    
+                f = open('binfileName','rb')
+                data = f.read()
+                return data
+            else:
+                print ">>>>>>>>>>>>>>>>Firmware found to be Up to date. No action needed."
+                return "Firmware found to be Up to date. No action needed.", 409
+        else:
+            print ">>>>>>>>>>>>>>>>>>Device not found in the list"
+            return "Device not found ", 404
     else:
-        return "No action available for this message type. Available message types are :" + "firmwareupdate"
+        print "Incorrect message type, no action available for this message type"
+        return "No action available for this message type. Available message types are :" + "firmwareupdate", 404
     
 
 if __name__ == '__main__':
